fb92338d6d01483be8bc52df1df39d62fc4dffee
Your Name <melissarburnham@gmail.com>
2018-03-01 20:13:17 -0500

    added delete button but not from database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,13 +12,14 @@ window.onload = function() {
   firebase.initializeApp(config);
 
   var database = firebase.database();
-
+  
   var trainName = "";
   var destination = "";
   var trainTime;
   var frequency = 0;
   var minutesAway;
   var nextArrival;
+  var trainCount = 0;
 
 $("#submit").on("click", function(){
     event.preventDefault();
@@ -27,6 +28,7 @@ $("#submit").on("click", function(){
     var destination = $("#destination").val().trim();
     var trainTime = $("#trainTime").val().trim();
     var frequency = $("#frequency").val().trim();
+    
 
     database.ref().push({
         name: trainName,
@@ -40,32 +42,26 @@ $("#submit").on("click", function(){
         $("input").val("");         
     }
 
-    // function calculateNextArrival(){
-    //     var trainTimeNum = parseInt(trainTime);
-    //     console.log(trainTimeNum);
-    // }
-   
-    // calculateNextArrival();
-    // alert("Train has successfully been submitted!");
-
     emptyInput();
     });
 
     database.ref().on("child_added", function(snapshot){
 
-        var tName = snapshot.val().name;
-        var tDestination = snapshot.val().destination;
-        var tTime = snapshot.val().firstTrain;
-        var tFrequency = snapshot.val().frequency;
-
-        console.log(snapshot.val());
-        console.log(snapshot.val().name);
-        console.log(snapshot.val().destination);
-        console.log(snapshot.val().firstTrian);
-        console.log(snapshot.val().frequency);
+        var sv = snapshot.val();
+        var tName = sv.name;
+        var tDestination = sv.destination;
+        var tTime = sv.firstTrain;
+        var tFrequency = sv.frequency;
+        
+        console.log(sv);
+        console.log(sv.name);
+        console.log(sv.destination);
+        console.log(sv.firstTrian);
+        console.log(sv.frequency);
 
         console.log("FirstTrain: " + tTime);
-
+        //CONVERTS AND CALCULATES TIME FIRST ARRIVAL...
+        //...AND MINUTES AWAY
         var tTimeConverted = moment(tTime, "HH:mm");
         console.log("TIME CONVERTED: " + tTimeConverted);
         var currentTime = moment();
@@ -76,25 +72,41 @@ $("#submit").on("click", function(){
         console.log("remainder: " + tRemainder);
         var minutesTillTrain = (tFrequency - tRemainder);
         console.log("minTilTrain: " + minutesTillTrain);
-        var nextTrain = moment().add(minutesTillTrain, "minutes").format("HH:mm");
+        var nextTrain = moment().add(minutesTillTrain, "minutes");
         console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
 
         var tBody = $("#trainTable");
         var tRow = $("<tr>");
         var trainTd = $("<td>").text(tName);
         var destinationTd = $("<td>").text(tDestination);
-        var trainTimeTd = $("<td>").text(nextTrain);
+        var trainTimeTd = $("<td>").text(nextTrain.format("LT"));
         var frequencyTd = $("<td>").text(tFrequency);
         var minutesAwayTd = $("<td>").text(minutesTillTrain);
+        var button = $("<td>").html($("<button>"));
+        button.attr("data-delete", trainCount);
+        button.addClass("deleteButton");
+        button.text("DELETE");
         
+        tRow.attr("id", "train-" + trainCount);
         // Append the newly created table data to the table row
-        tRow.append(trainTd, destinationTd, frequencyTd, trainTimeTd, minutesTillTrain);
+        tRow.append(trainTd, destinationTd, frequencyTd, trainTimeTd, minutesTillTrain, button);
         // Append the table row to the table body
         tBody.append(tRow);
-    
+
+        trainCount++;
+
+        $(document.body).on("click", ".deleteButton", function() {
+            var trainNumber = $(this).attr("data-delete");
+            
+            $("#train-" + trainNumber).remove();  
+
+          });
+
     }, function(errorObject){
         console.log("The read failed: " + errorObject.code)      
     })
+    
+   
 }
 
 //NEED TO DO:
