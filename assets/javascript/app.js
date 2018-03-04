329178bed459ee01db1b75fec5fd8b1e4e9e0263
Your Name <melissarburnham@gmail.com>
2018-03-04 09:37:16 -0500

    trying signin

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -52,6 +52,32 @@ var uiConfig = {
     ui.start('#firebaseui-auth-container', uiConfig);
 
 
+    var provider = new firebase.auth.GithubAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+
+      firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+      }).catch(function(error) {
+        // An error happened.
+      });
+
+
 
 
 // $(".container").hide();
