d78ceec3969392febed0ccd22b4bd32315ba2962
Your Name <melissarburnham@gmail.com>
2018-03-04 09:59:46 -0500

    finally figured out sign in

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,27 +30,19 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
     callbacks: {
       signInSuccess: function(currentUser, credential, redirectUrl) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
         return true;
-      },
-      uiShown: function() {
-        // The widget is rendered.
-        // Hide the loader.
-        document.getElementById('loader').style.display = 'none';
       }
     },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+
     signInFlow: 'popup',
     signInSuccessUrl: '<https://melissarburnham.github.io/Train-Scheduler/>',
     signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
+      
       firebase.auth.GithubAuthProvider.PROVIDER_ID
     ]
   };
 
-  // The start method will wait until the DOM is loaded.
+ 
     ui.start('#firebaseui-auth-container', uiConfig);
 
     $(".login").on("click", function(){
@@ -58,50 +50,28 @@ var uiConfig = {
     var provider = new firebase.auth.GithubAuthProvider();
 
     firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
         $(".container").show();
+        $(".welcome").hide();
+        $(".login").hide();
+        console.log(result.user);
         // ...
       }).catch(function(error) {
-        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        // The email of the user's account used.
         var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
         // ...
       });
 
       firebase.auth().signOut().then(function() {
-        // Sign-out successful.
       }).catch(function(error) {
-        // An error happened.
       });
 
       
       });
-
-
-// function login(){
-   
-//     var provider = new firebase.auth.GithubAuthProvider();
-//     firebase.auth().signInWithPopup(provider).then(function(result) {
-//         var token = result.credential.accessToken;
-//         var user = result.user;
-
-//         console.log(user);
-//       }).catch(function(error) {
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//         var email = error.email;
-//         var credential = error.credential;
-//         console.log(errorMessage);
-//       });
-//   }
-    
 
 $("#submit").on("click", function(){
     event.preventDefault();
